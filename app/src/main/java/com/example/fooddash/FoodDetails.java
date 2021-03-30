6c8a4cf9210f331b1e953fee03cdad6b7b142e32
santiruz <68023672+santiruz@users.noreply.github.com>
2021-03-29 20:47:59 -0400

    added cart

--- app/src/main/java/com/example/fooddash/FoodDetails.java
+++ app/src/main/java/com/example/fooddash/FoodDetails.java
@@ -4,16 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class FoodDetails extends AppCompatActivity {
 
-    ImageView imageView;
+    ImageView imageView,backButton;
     TextView itemName, itemPrice, itemRating;
     RatingBar ratingBar;
+    ListView listView;
+    static ArrayAdapter<String> adapter;
+    ArrayList<String> listItems=new ArrayList<String>();
+
 
     String name, price, rating, imageUrl;
 
@@ -34,11 +45,35 @@ public class FoodDetails extends AppCompatActivity {
         itemPrice = findViewById(R.id.price);
         itemRating = findViewById(R.id.rating);
         ratingBar = findViewById(R.id.ratingBar);
+        backButton = findViewById(R.id.imageView2);
 
         itemName.setText(name);
-        itemPrice.setText("$ "+price);
+        itemPrice.setText(price);
         itemRating.setText(rating);
         ratingBar.setRating(Float.parseFloat(rating));
+
+    }
+
+    public void goBack(View v)
+    {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+    }
+    public void shoppingCart(View v)
+    {
+    Intent intent = new Intent(this,ShoppingCart.class);
+        startActivity(intent);
+    }
+    public void addToCart(View v)
+    {
+       // listView = (ListView) findViewById(R.id.list_users);
+        adapter =new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+        listItems.add(name + "\t\t" + price);
+        adapter.notifyDataSetChanged();
+        Toast.makeText(FoodDetails.this, "Added to cart", Toast.LENGTH_SHORT).show();
+
+
+
 
     }
 }
--- app/src/main/java/com/example/fooddash/MainActivity.java
+++ app/src/main/java/com/example/fooddash/MainActivity.java
@@ -1,24 +1,9 @@
 package com.example.fooddash;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import java.util.ArrayList;
-import java.util.List;
-
-
-import com.example.fooddash.adapter.PopularAdapter;
-import com.example.fooddash.adapter.RecommendedAdapter;
-import com.example.fooddash.model.Menu;
-import com.example.fooddash.adapter.MenuAdapter;
-import com.example.fooddash.model.Popular;
-import com.example.fooddash.model.Recommended;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, SignUpActivity.class);
+        Intent intent = new Intent(this, Home.class);
         startActivity(intent);
 
     }
